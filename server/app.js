574e4de94da3a774ee700cda34cabb88ec4981d1
efecollins <efejamb@gmail.com>
2023-01-18 11:57:25 +0100

    incomplete setup

---
+++ server/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
