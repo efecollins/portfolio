c0c91eaf875be15243be2121f951e2f64d3be62f
efecollins <efejamb@gmail.com>
2023-01-22 11:09:47 +0100

    setup @ 50% percent

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="App bg-[#0d1117] min-h-screen text-[#aab1b9]">
+      <header className="App-header">
+      <nav>
+        <ul>
+          <li>Efosa</li>
+        </ul>
+      </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 require('dotenv').config()
+
+const port = process.env.PORT | 1000
+
 const app = express()
-const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
